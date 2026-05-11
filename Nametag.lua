1ef6318cf734ee7957057d9add5b3d0f049a24a1
67676767676767-code <caitlynisom15@icloud.com>
2026-05-11 04:08:26 -0400

    Create Nametag.lua

---
+++ Nametag.lua
@@ -0,0 +1,19 @@
+{
+  "player_id": {
+    "10927326118": {
+      "decal_id": "rbxassetid://123456789",
+      "custom_name": "nigger",
+      "color": "#FF19028"
+    },
+    "1002": {
+      "decal_id": "rbxassetid://987654321",
+      "custom_name": "Blaze",
+      "color": "#00FFFF"
+    },
+    "1003": {
+      "decal_id": "rbxassetid://111222333",
+      "custom_name": "Ghost",
+      "color": "#FFFFFF"
+    }
+  }
+}
